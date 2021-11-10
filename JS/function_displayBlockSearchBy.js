2936219e334e227a8f2f565d90e4d4aa96e63031
fanjatiana.pavageau <fanjatiana.pavageau@outlook.com>
2021-11-10 16:54:28 +0100

    agrandissement des blocs au survole

--- JS/function_displayBlockSearchBy.js
+++ JS/function_displayBlockSearchBy.js
@@ -1,8 +1,10 @@
-import { blockSubMenuIngredients,blockSubMenuAppliances,blockSubMenuTools } from "./const.js";
+import { blockSubMenuIngredients,blockSubMenuAppliances,blockSubMenuTools, searchBarByIngredients, searchBarByAppliances, searchBarByTools } from "./const.js";
 
 // fonction pour afficher le bloc contenant la liste des ingrédients
 
-
+let blockIngredient = document.getElementById("by_ingredients");
+let blockAppliance = document.getElementById("kitchen-appliance");
+let blockTool = document.getElementById("kitchen-tool");
 let chevronBlockIngredient = document.querySelector(".chevron__block-ingredient");
 let chevronBlockAppliance = document.querySelector(".chevron__block-appliance");
 console.log(chevronBlockAppliance)
@@ -11,21 +13,31 @@ console.log(chevronBlockTool)
 
 export const displayBlockSearchByIngredients = () => {
   blockSubMenuIngredients.style.display = "block";
+  blockIngredient.style.width="50%";
+  blockIngredient.style.height="900px";
+  searchBarByIngredients.style.height = "80px"
   chevronBlockIngredient.classList.add("rotate");
+
 };
 export const displayNoneSearchByIngredients = () => {
   blockSubMenuIngredients.style.display = "none";
+  blockIngredient.style.width="20%";
+  blockIngredient.style.height="100px";
   chevronBlockIngredient.classList.remove("rotate");
 };
 
 
 export const displayBlockSearchByAppliances = () => {
-
   blockSubMenuAppliances.style.display = "block";
   chevronBlockAppliance.classList.add("rotate");
+  blockAppliance.style.width="20%";
+  blockAppliance.style.height="500px";
+  searchBarByAppliances.style.height = "80px"
 };
 export const displayNoneSearchByAppliances = () => {
   blockSubMenuAppliances.style.display = "none";
+  blockAppliance.style.width="20%";
+  blockAppliance.style.height="100px";
   chevronBlockAppliance.classList.remove("rotate");
 };
 
@@ -33,10 +45,15 @@ export const displayBlockSearchByTools = () => {
 
   blockSubMenuTools.style.display = "block";
   chevronBlockTool.classList.add("rotate");
+  blockTool.style.width="30%";
+  blockTool.style.height="600px";
+  searchBarByTools.style.height = "80px"
 };
 export const displayNoneSearchByTools = () => {
   blockSubMenuTools.style.display = "none";
   chevronBlockTool.classList.remove("rotate");
+  blockTool.style.width="20%";
+  blockTool.style.height="100px";
 };
 
 
